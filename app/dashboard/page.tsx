d46d395407e341cf14033b3fdff35338ffa6999e
Khushal Khandelwal <khandelwalkhushal79@gmail.com>
2024-06-22 23:55:34 +0530

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Dashboard() {
+    return (
+        <main>
+            <h1>Hi this is dashboard</h1>
+        </main>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import {ModeToggle} from "@/app/components/theme-toggle";
+import Navbar from "@/app/components/navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hi this is my To do app</h1>
+    </main>
+  );
+}
